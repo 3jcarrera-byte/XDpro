895dbd4435207e4e94b16b4bae019cb646dda2ee
3jcarrera-byte <3jcarrera@gmail.com>
2026-09-03 16:33:48 -0400

    Update game3d.js

--- public/js/game3d.js
+++ public/js/game3d.js
@@ -154,11 +154,11 @@ function generarCimientos(containerId, cantidad) {
 
     if (containerId === 'canvas-finca-container') {
         const posicionesCimientosFinca = [
-            { x: -3.5, z:  0.5 }, // Cimiento 0: Izquierdo
+            { x: -7.0, z:  8.0 }, // Cimiento 0: Izquierdo
             { x:  0.0, z:  1.5 }, // Cimiento 1: Central izquierdo
             { x:  2.5, z:  1.5 }, // Cimiento 2: Central derecho
             { x:  0.0, z: -2.5 }, // Cimiento 3: Superior
-            { x:  4.5, z:  3.5 }  // Cimiento 4: Inferior derecho externo
+            { x:  7.0, z:  8.0 }  // Cimiento 4: Inferior derecho externo
         ];
 
         posicionesCimientosFinca.forEach((pos, i) => {
